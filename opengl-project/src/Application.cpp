8bb3975be8abd3a101363f2594e448cb9cddac7b
Dmitry <dmitriy.bagandov@gmail.com>
2020-05-10 23:41:07 +0300

    Add glew

--- opengl-project/src/Application.cpp
+++ opengl-project/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +21,12 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    GLenum status = glewInit();
+    if (status != GLEW_OK)
+    {
+        std::cout << "Error from glewInit()" << std::endl;
+    }
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
